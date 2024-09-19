31f3e6b10ee7b5935b9f7faf79da7934c964937a
DashieTM <fabio.lenherr@gmail.com>
2024-09-19 22:06:36 +0200

    Create basic box

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use iced::widget::{column, text, Column};
-use iced::{event, Alignment, Element, Event, Length, Task, Theme};
+use iced::theme::palette;
+use iced::widget::container::Style;
+use iced::widget::{column, container, text, Column};
+use iced::{event, Alignment, Element, Event, Length, Subscription, Task, Theme};
 use theme::{get_all_themes, get_theme};
 use widgets::oxi_button::{button, ButtonVariant};
 use widgets::oxi_checkbox::checkbox;
@@ -12,7 +14,7 @@ use widgets::oxi_text_input::text_input;
 use widgets::oxi_toggler::toggler;
 
 use iced_layershell::actions::LayershellCustomActions;
-use iced_layershell::reexport::Anchor;
+use iced_layershell::reexport::{Anchor, Layer};
 use iced_layershell::settings::{LayerShellSettings, Settings};
 use iced_layershell::Application;
 
@@ -23,10 +25,12 @@ mod widgets;
 pub fn main() -> Result<(), iced_layershell::Error> {
     let settings = Settings {
         layer_settings: LayerShellSettings {
-            size: Some((0, 400)),
-            exclusive_zone: 400,
-            anchor: Anchor::Bottom | Anchor::Left | Anchor::Right,
+            size: Some((600, 600)),
+            exclusive_zone: 0,
+            anchor: Anchor::Left | Anchor::Right,
             binded_output_name: Some("pingpang".into()),
+            layer: Layer::Overlay,
+            margin: (100, 100, 100, 100),
             ..Default::default()
         },
         ..Default::default()
@@ -77,34 +81,56 @@ enum Message {
     Check(),
     Toggle(bool),
     Theme(Theme),
-
-    TextInput(String),
-    Direction(WindowDirection),
-    SizeChange((u32, u32)),
-    IcedEvent(Event),
+    //TextInput(String),
+    //Direction(WindowDirection),
+    //SizeChange((u32, u32)),
+    //IcedEvent(Event),
 }
 impl TryInto<LayershellCustomActions> for Message {
     type Error = Self;
     fn try_into(self) -> Result<LayershellCustomActions, Self::Error> {
-        match self {
-            Self::Direction(direction) => Ok(match direction {
-                WindowDirection::Left => LayershellCustomActions::AnchorChange(
-                    Anchor::Left | Anchor::Top | Anchor::Bottom,
-                ),
-                WindowDirection::Top => LayershellCustomActions::AnchorChange(
-                    Anchor::Top | Anchor::Left | Anchor::Right,
-                ),
-                WindowDirection::Right => LayershellCustomActions::AnchorChange(
-                    Anchor::Top | Anchor::Bottom | Anchor::Right,
-                ),
-                WindowDirection::Bottom => LayershellCustomActions::AnchorChange(
-                    Anchor::Bottom | Anchor::Left | Anchor::Right,
-                ),
-            }),
-            Self::SizeChange((x, y)) => Ok(LayershellCustomActions::SizeChange((x, y))),
-            _ => Err(self),
-        }
+        Err(self)
     }
+}
+//match self {
+//    Self::Direction(direction) => Ok(match direction {
+//        WindowDirection::Left => LayershellCustomActions::AnchorChange(
+//            Anchor::Left | Anchor::Top | Anchor::Bottom,
+//        ),
+//        WindowDirection::Top => LayershellCustomActions::AnchorChange(
+//            Anchor::Top | Anchor::Left | Anchor::Right,
+//        ),
+//        WindowDirection::Right => LayershellCustomActions::AnchorChange(
+//            Anchor::Top | Anchor::Bottom | Anchor::Right,
+//        ),
+//        WindowDirection::Bottom => LayershellCustomActions::AnchorChange(
+//            Anchor::Bottom | Anchor::Left | Anchor::Right,
+//        ),
+//    }),
+//    Self::SizeChange((x, y)) => Ok(LayershellCustomActions::SizeChange((x, y))),
+//    _ => Err(self),
+//}
+//        Err(self)
+//    }
+//}
+fn box_style(theme: &Theme) -> Style {
+    let palette = theme.extended_palette();
+    Style {
+        background: Some(iced::Background::Color(palette.background.base.color)),
+        border: iced::border::rounded(10),
+        ..container::rounded_box(theme)
+    }
+}
+
+fn wrap_in_rounded_box<'a>(
+    content: impl Into<Element<'a, Message, Theme>>,
+) -> Element<'a, Message> {
+    container(content)
+        .style(box_style)
+        .align_x(Alignment::Center)
+        .padding(50)
+        .max_width(550)
+        .into()
 }
 
 impl Application for Counter {
@@ -123,12 +149,14 @@ impl Application for Counter {
     }
 
     fn namespace(&self) -> String {
-        String::from("Counter - Iced")
+        String::from("Oxiced")
     }
 
     fn subscription(&self) -> iced::Subscription<Self::Message> {
-        event::listen().map(Message::IcedEvent)
+        //event::listen().map(Message::IcedEvent)
+        Subscription::none()
     }
+
     fn update(&mut self, message: Message) -> Task<Message> {
         match message {
             Message::Slider(val) => {
@@ -149,27 +177,53 @@ impl Application for Counter {
                 self.text += "1";
             }
             Message::TextChanged(val) => self.text = val,
-            _ => (),
+            //_ => (),
         };
         Task::none()
     }
 
     fn view(&self) -> Element<Message> {
-        column![
-            counter_box(self),
-            pick_list(get_all_themes(), Some(&self.theme), Message::Theme).width(Length::Fill),
-        ]
-        .padding(20)
-        .align_x(Alignment::Center)
-        .into()
+        wrap_in_rounded_box(
+            column![
+                testing_box_2(self),
+                pick_list(get_all_themes(), Some(&self.theme), Message::Theme).width(Length::Fill),
+            ]
+            .padding(20)
+            .max_width(530)
+            .align_x(Alignment::Center),
+        )
     }
 
     fn theme(&self) -> Theme {
         self.theme.clone()
     }
+
+    // remove the annoying background color
+    fn style(&self, theme: &Self::Theme) -> iced_layershell::Appearance {
+        let palette = theme.extended_palette();
+        iced_layershell::Appearance {
+            background_color: iced::Color::TRANSPARENT,
+            text_color: palette.background.base.text,
+        }
+    }
+
+    fn scale_factor(&self) -> f64 {
+        1.0
+    }
 }
 
-fn counter_box<'a>(state: &Counter) -> Column<'a, Message> {
+fn testing_box_2<'a>(state: &Counter) -> Column<'a, Message> {
+    column![
+        button("Increment", ButtonVariant::Primary).on_press(Message::Increment(10)),
+        text(state.value).size(50),
+        button("Decrement", ButtonVariant::Secondary).on_press(Message::Decrement(20)),
+    ]
+    .padding(20)
+    .max_width(500)
+    .align_x(Alignment::Center)
+}
+
+fn testing_box<'a>(state: &Counter) -> Column<'a, Message> {
     column![
         button("Increment", ButtonVariant::Primary).on_press(Message::Increment(10)),
         text(state.value).size(50),
@@ -189,5 +243,6 @@ fn counter_box<'a>(state: &Counter) -> Column<'a, Message> {
         vertical_rule(100),
     ]
     .padding(20)
+    .max_width(500)
     .align_x(Alignment::Center)
 }
